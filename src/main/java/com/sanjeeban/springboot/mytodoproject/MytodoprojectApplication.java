4e26e2d59af783bf0398c3cd43daf0d03785f314
Sanjeeban <sanjeeban5644@gmail.com>
2023-10-08 11:13:00 +0530

    Project initialized

---
+++ src/main/java/com/sanjeeban/springboot/mytodoproject/MytodoprojectApplication.java
@@ -0,0 +1,12 @@
+package com.sanjeeban.springboot.mytodoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytodoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytodoprojectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sanjeeban/springboot/mytodoproject/MytodoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjeeban.springboot.mytodoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytodoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
